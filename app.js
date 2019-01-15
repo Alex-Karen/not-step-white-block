03a14cbba267d4c17f75f259863b9c832575f3be
xfh <m13928810941@163.com>
2019-01-15 14:05:09 +0800

    add index code

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+    onLaunch: function () {
+        // 展示本地存储能力
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
+    },
+    getUserInfo: function (cb) {
+        var that = this;
+        if (this.globalData.userInfo) {
+            typeof cb == "function" && cb(this.globalData.userInfo)
+        } else {
+            wx.login({
+                success: function () {
+                    wx.getUserInfo({
+                        success: function (result) {
+                            that.globalData.userInfo = result.userInfo
+                            typeof cb == "function" && cb(that.globalData.userInfo)
+                        }
+                    })
+                }
+            })
         }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+    },
+    getHeighestScore: function (scoreName, cb) {
+        this.globalData[scoreName] = wx.getStorageSync(scoreName)
+        typeof cb == "function" && cb(this.globalData[scoreName])
+    },
+    globalData: {
+        userInfo: null,
+        currentScore: 0,
+        endlessScore: null,
+        timeScore: null,
+        speedScroe: null
+    }
 })
